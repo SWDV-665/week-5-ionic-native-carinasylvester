aa4e227f873e1cdb4b7e769f7a73e7c243ca5139
carinasylvester <carina.sylvester@outlook.com>
2021-07-17 09:04:55 -0400

    Added Angular elements for grocery items

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,26 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  title = "Grocery List";
 
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Bread",
+      quantity: 1
+    },
+  {
+      name: "Eggs",
+      quantity: 12
+    },
+  {
+      name: "Banana",
+      quantity: 3
+    },
+  ];
   constructor(public navCtrl: NavController) {
 
   }
